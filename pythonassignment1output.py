f27ddf87af2e4c2cb41d237ec1fd297a667d4876
ChiraggargCSE <96727681+ChiraggargCSE@users.noreply.github.com>
2022-01-08 13:43:06 +0530

    Add files via upload

---
+++ pythonassignment1output.py
@@ -0,0 +1,39 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Q1
+Number 1 : 4
+Number 2 : 6
+Number 3 : 2
+Average: 4.0
+Q2
+Enter your gross income(in $): 100000
+Enter number of dependents: 2
+Taxable income: 84000.0
+Tax: 16800.0
+Q3
+SID: 2
+Name: ch
+Gender: m
+Course name: cse
+CGPA: 5.7
+['2', 'ch', 'm', 'cse', 5.7]
+Q4
+Enter marks of student 1: 3
+Enter marks of student 2: 9
+Enter marks of student 3: 0
+Enter marks of student 4: 6
+Enter marks of student 5: 1
+[0.0, 1.0, 3.0, 6.0, 9.0]
+Q5a
+Colour: 
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+After removing element
+['Red', 'White', 'Black', 'Pink', 'Yellow']
+Q5b
+Colour: 
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+ After editing: 
+['Red', 'Green', 'White', 'Yellow', 'Purple']
+>>> 
