d2fb27d70e4318ae9dcb0d667f5e9e350a9c5b23
ChiraggargCSE <96727681+ChiraggargCSE@users.noreply.github.com>
2022-02-13 22:24:37 +0530

    Add files via upload

---
+++ pythonassignment3output.py
@@ -0,0 +1,94 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:\Users\chira\OneDrive\Desktop\Python\pythonassignment3.py =====
+Give a string-yfigjhydytdhkihklkih
+{'y': 3, 'f': 1, 'i': 3, 'g': 1, 'j': 1, 'h': 4, 'd': 2, 't': 1, 'k': 3, 'l': 1}
+Give month-4
+Give day-13
+Give year-2021
+Date- 13 / 4 / 2021
+Next Date- 14 / 4 / 2021
+Done!
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+Give a number between 4 and 10 including them-6
+Performance=Average
+Letter Grade=C+
+Done!
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+----------------(a)-------------------
+Give the sid (a number):21103026
+Enter your name:Chirag
+give a letter y or n:y
+Give the sid (a number):21103026
+Enter your name:Chirag
+give a letter y or n:n
+The required dictionary-- {21103026: 'Chirag'}
+The list will be used in further parts--
+The required list-- [(21103026, 'Chirag'), (21103026, 'Chirag')]
+Done!
+----------------(b)------------------------
+The dictionary we had-
+{21103026: 'Chirag'}
+now we have exchanged the key value pair but its not sorted-
+{'Chirag': 21103026}
+The unsorted list--
+[('Chirag', 21103026)]
+The sorted list--
+[('Chirag', 21103026)]
+The sorted dictionary-
+{'Chirag': 21103026}
+This is the dictionary sorted on the basis of name-
+{21103026: 'Chirag'}
+Done!
+-----------------(c)-----------------
+The list we made before will be used now-
+[(21103026, 'Chirag'), (21103026, 'Chirag')]
+now the list is sorted-
+[(21103026, 'Chirag'), (21103026, 'Chirag')]
+sorted dictionary based on sid- {21103026: 'Chirag'}
+Done!
+-----------------(d)-----------------
+Please enter one of the sids you entered before-21103026
+The name of the student with the entered sid is-
+Chirag
+Done!
+Give a number-4
+Give a number-5
+[4, 5, 9]
+Give y to contnue and n to stop going further-y
+[4, 5, 9, 14]
+Give y to contnue and n to stop going further-y
+[4, 5, 9, 14, 23]
+Give y to contnue and n to stop going further-y
+[4, 5, 9, 14, 23, 37]
+Give y to contnue and n to stop going further-y
+[4, 5, 9, 14, 23, 37, 60]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[4, 5, 9, 14, 23, 37, 60]
+Average of the sequence-
+21.714285714285715
+Done!
+-------------(a)------------
+{1, 3, 5, 6, 8}
+Done!
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+Done!
+-------------(c)------------
+{1, 2, 4, 5}
+Done!
+-------------(d)------------
+{6, 7, 8, 9, 10}
+Done!
+-------------(e)------------
+{10, 7}
+Done!
