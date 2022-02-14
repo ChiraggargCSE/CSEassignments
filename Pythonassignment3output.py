d551a4008b6634f8737f8f9b4dacec155a87e79a
ChiraggargCSE <96727681+ChiraggargCSE@users.noreply.github.com>
2022-02-14 13:10:52 +0530

    Add files via upload

---
+++ Pythonassignment3output.py
@@ -0,0 +1,104 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:\Users\chira\OneDrive\Desktop\Python\pythonassignment3.py =====
+Give a string-nxoqnjlckq
+{'n': 2, 'x': 1, 'o': 1, 'q': 2, 'j': 1, 'l': 1, 'c': 1, 'k': 1}
+Give month-2
+Give year-2022
+Give day-28
+Date- 28 / 2 / 2022
+Next Date- 1/ 3 / 2022
+Done!
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64)]
+Give a number between 4 and 10 including them-7
+Performance=Good
+Letter Grade=B
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+----------------(a)-------------------
+Give the sid (a number):21103026
+Enter your name:chirag
+give a letter y or n:y
+Give the sid (a number):21103028
+Enter your name:aakash
+give a letter y or n:n
+The required dictionary-- {21103026: 'chirag', 21103028: 'aakash'}
+The list will be used in further parts--
+The required list-- [(21103026, 'chirag'), (21103028, 'aakash')]
+----------------(b)------------------------
+The dictionary we had-
+{21103026: 'chirag', 21103028: 'aakash'}
+now we have exchanged the key value pair but its not sorted-
+{'chirag': 21103026, 'aakash': 21103028}
+The unsorted list--
+[('chirag', 21103026), ('aakash', 21103028)]
+The sorted list--
+[('aakash', 21103028), ('chirag', 21103026)]
+The sorted dictionary-
+{'aakash': 21103028, 'chirag': 21103026}
+This is the dictionary sorted on the basis of name-
+{21103028: 'aakash', 21103026: 'chirag'}
+-----------------(c)-----------------
+The list we made before will be used now-
+[(21103026, 'chirag'), (21103028, 'aakash')]
+now the list is sorted-
+[(21103026, 'chirag'), (21103028, 'aakash')]
+sorted dictionary based on sid- {21103026: 'chirag', 21103028: 'aakash'}
+-----------------(d)-----------------
+Please enter one of the sids you entered before-21103026
+The name of the student with the entered sid is-
+chirag
+Give a number-4
+Give a number-2
+[4, 2, 6]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042, 1686]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042, 1686, 2728]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042, 1686, 2728, 4414]
+Give y to contnue and n to stop going further-y
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042, 1686, 2728, 4414, 7142]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[4, 2, 6, 8, 14, 22, 36, 58, 94, 152, 246, 398, 644, 1042, 1686, 2728, 4414, 7142]
+Average of the sequence-
+1038.6666666666667
+-------------(a)------------
+{1, 3, 5, 6, 8}
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+-------------(c)------------
+{1, 2, 4, 5}
+-------------(d)------------
+{6, 7, 8, 9, 10}
+-------------(e)------------
+{10, 7}
+
