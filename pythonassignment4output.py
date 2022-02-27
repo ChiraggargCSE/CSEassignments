2d197c50aa518fc0c7b1fdcc2b6cbb87202ce7f3
ChiraggargCSE <96727681+ChiraggargCSE@users.noreply.github.com>
2022-02-27 18:20:32 +0530

    Add files via upload

---
+++ pythonassignment4output.py
@@ -0,0 +1,64 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/chira/OneDrive/Desktop/Python/pythonassignment4.py =====
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 7
+USING LOOPS
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+
+
+
+USING RECURSSIONS
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+
+ANSWER 3
+Enter 2 numbers: 9 6
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [5, 7, 8, 6, 8, 7, 9]
+<d> Set: {5, 6, 7, 8, 9}
+<e> Immutable set: frozenset({5, 6, 7, 8, 9})
+<f> Hash value of the max value from the above set: 9
+
+
+ANSWER 4 
+Object created
+The name of the student it Vaishnav and SID is 21103016.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+ANSWER 6
+Enter the word: lead
+Enter a new meaningful word using the exact same letters to test your friendship: deal
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
